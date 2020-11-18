04f6fa84ccbb4ee0899e39d508ef7f8bdf0d3ea3
unknown <rohituikey81@gmail.com>
2020-11-18 01:54:37 -0800

    second commit

--- movieway/settings.py
+++ movieway/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,5 @@ STATIC_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
